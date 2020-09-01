7e72afafa7a3c2042f5b78d86c2e392009522509
Haider Ali Punjabi <haiderali@cyberservices.com>
2020-09-01 16:19:24 +0530

    Bump 0.0.3 -> 0.0.4

--- cli_badges/__init__.py
+++ cli_badges/__init__.py
@@ -2,5 +2,5 @@ from __future__ import print_function
 
 from .cli_badges import *
 
-__version_info__ = (0, 0, 3)
+__version_info__ = (0, 0, 4)
 __version__ = '{0}.{1}.{2}'.format(*__version_info__)
